b36140bbc369840ef66e0e328d2e64ea91e97ef5
Renzo Conti <contiantunezrenzo@gmail.com>
2023-01-28 21:03:14 -0300

    Agrego primer menu

--- script.js
+++ script.js
@@ -2,6 +2,20 @@ alert("Bienvenido al Almacén de Bebidas!\nPresione aceptar para iniciar su pedi
 
 let fin = false;
 let carrito = "";
+let precio = 0;
+
+function pedirYValidarInt(menor, mayor, descripcion) {
+    let dato; 
+    do{
+        dato = parseInt(prompt(descripcion))
+        if (dato<menor || dato>mayor || isNaN(dato)){
+            alert("El valor ingresado está fuera del rango de opciones, intente nuevamente.");
+        }
+    } while (dato<menor || dato>mayor);
+    return dato;
+}
+
+//Loop principal
 
 while (!fin) {
     let opMenu = pedirYValidarInt(1,3,"Ingrese el número para acceder a la opción que desea del menú:\n1-Bebidas\n2-Ver carrito\n3-Finalizar compra")
@@ -13,20 +27,10 @@ while (!fin) {
             alert(`Su carrito contiene:\n ${carrito}`)
             break;
         case 3:
+            alert("")
             fin = true;
             break;
         default:
             break;
     }
-}
-
-function pedirYValidarInt(menor, mayor, descripcion) {
-    let dato; 
-    do{
-        dato = parseInt(prompt(descripcion))
-        if (dato<menor || dato>mayor || isNaN(dato)){
-            alert("El valor ingresado está fuera del rango de opciones, intente nuevamente.");
-        }
-    } while (dato<menor || dato>mayor);
-    return dato;
 }
